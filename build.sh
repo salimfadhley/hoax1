771221dd84dc59bfe9cb5ff2853201dd2713ed9c
Salim Fadhley <salimfadhley@gmail.com>
2020-10-12 21:33:34 +0100

    Fix some spellings

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 mkdir -p ./project/out
 docker-compose run python make_message.py
-docker-compose run qr -lH -o qrcode.png -r message.txt
+docker-compose run qr -lM -o qrcode.png -r message.txt
 docker-compose run convert qrcode.png qrcode.jpg
 docker-compose run xelatex -output-directory=/project/out bisturbile_defence_matrix.tex
