c48c1721f2882a4e8e242067314f793a28f4bfc0
Salim Fadhley <salimfadhley@gmail.com>
2020-10-12 22:09:15 +0100

    More typos, changed the message

--- tex/make_message.py
+++ tex/make_message.py
@@ -3,7 +3,7 @@ import codecs
 rot13 = codecs.getencoder( "rot-13" )
 
 
-CLEARTEXT_MESSAGE:bytes = rot13("Mark, how gullible can you be? 5G is not a weapon. Go back to plumbing.")[0].encode("utf-8")
+CLEARTEXT_MESSAGE:bytes = rot13("Mark Steele is a gullible fuckwit!")[0].encode("utf-8")
 
 with open("message.txt", "w") as f:
     f.write(base64.b64encode(CLEARTEXT_MESSAGE).decode("utf-8"))
