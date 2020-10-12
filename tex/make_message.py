f1b537f9016db91562d1a25b49ef9257fa521422
Salim Fadhley <salimfadhley@gmail.com>
2020-10-12 09:29:33 +0100

    Some grammar fixes

--- tex/make_message.py
+++ tex/make_message.py
@@ -1,6 +1,6 @@
 import base64
 
-CLEARTEXT_MESSAGE = b"Mark Steele is the biggest idiot of all time."
+CLEARTEXT_MESSAGE:bytes = "Mark, how gullible can you be? 5G is not a weapon. Go back to plumbing.".encode("utf-8")
 
 with open("message.txt", "w") as f:
     f.write(base64.b64encode(CLEARTEXT_MESSAGE).decode("utf-8"))
