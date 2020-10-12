b0745e6802492ea3534b8ceb8c589e42700d3528
Salim Fadhley <salimfadhley@gmail.com>
2020-10-12 21:57:06 +0100

    Fix the apostraphe, improve the message

--- tex/make_message.py
+++ tex/make_message.py
@@ -1,6 +1,9 @@
 import base64
+import codecs
+rot13 = codecs.getencoder( "rot-13" )
 
-CLEARTEXT_MESSAGE:bytes = "Mark, how gullible can you be? 5G is not a weapon. Go back to plumbing.".encode("utf-8")
+
+CLEARTEXT_MESSAGE:bytes = rot13("Mark, how gullible can you be? 5G is not a weapon. Go back to plumbing.")[0].encode("utf-8")
 
 with open("message.txt", "w") as f:
     f.write(base64.b64encode(CLEARTEXT_MESSAGE).decode("utf-8"))
